00ead2694af3823b2d043cf5a41d462cc8066548
lionc2240 <pentakill2240@gmail.com>
2026-06-04 16:09:17 +0000

    Remove libcairo from emulated to fix XCB symbol errors

--- install.sh
+++ install.sh
@@ -297,7 +297,7 @@ cat <<EOF > "$VSF_DIR/VideoSubFinderWXW.run"
 #!/bin/sh
 export LD_LIBRARY_PATH="$LIBS_DIR:\$PWD:\$LD_LIBRARY_PATH"
 export BOX64_LD_LIBRARY_PATH="$LIBS_DIR:\$PWD:/usr/lib/x86_64-linux-gnu"
-export BOX64_EMULATED_LIBS="libOpenCL.so.1:libOpenCL.so:libgomp.so.1:libgomp.so:libgpg-error.so.0:libgcrypt.so.20:libavcodec.so.58:libavformat.so.58:libavutil.so.56:libswresample.so.3:libswscale.so.5:libavfilter.so.7:libglib-2.0.so.0:libgobject-2.0.so.0:libgio-2.0.so.0:libpango-1.0.so.0:libcairo.so.2:libgdk_pixbuf-2.0.so.0:libpangocairo-1.0.so.0:libpangoft2-1.0.so.0"
+export BOX64_EMULATED_LIBS="libOpenCL.so.1:libOpenCL.so:libgomp.so.1:libgomp.so:libgpg-error.so.0:libgcrypt.so.20:libavcodec.so.58:libavformat.so.58:libavutil.so.56:libswresample.so.3:libswscale.so.5:libavfilter.so.7:libglib-2.0.so.0:libgobject-2.0.so.0:libgio-2.0.so.0:libpango-1.0.so.0:libpangoft2-1.0.so.0"
 if [ -z "\$DISPLAY" ]; then
     xvfb-run -a box64 ./VideoSubFinderWXW "\$@"
 else
